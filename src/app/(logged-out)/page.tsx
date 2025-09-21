314e4a47b0cd9282d198c3af6e5db1d570ddf551
Govind Dandekar <govind.dandekar@gmail.com>
2025-09-21 16:52:59 -0700

    add Buttons

--- src/app/(logged-out)/page.tsx
+++ src/app/(logged-out)/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button"
+
 export default function LandingPage(){
     return (
         <div>
             <h1>Support Me</h1>
+            <Button>
+                Log in
+            </Button>
+            <Button>
+                Sign up
+            </Button>
         </div>
     )
 }
